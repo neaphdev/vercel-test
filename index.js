76cae28d84473d39b2f90ae48772e3dfe877df2b
Apfata <apfata.developer@gmail.com>
2024-04-14 23:42:40 -0500

    generate token random with random time

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+const crypto = require('crypto');
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+
+app.post("/login", (req, res) => {
+    // Generate random token
+    const token = crypto.randomBytes(64).toString('hex');
+
+    // Random sleep time up to 1 second
+    const sleepTime = Math.random() * 1000;
+
+    setTimeout(() => {
+        res.send({ token: token });
+    }, sleepTime);
+});
+
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
