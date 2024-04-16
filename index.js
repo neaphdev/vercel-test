912ac9c39fff8ffda35a9c4761bc0058851270b0
Apfata <apfata.developer@gmail.com>
2024-04-16 00:22:29 -0500

    adapt response to generated token

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 
 const app = express();
-const crypto = require('crypto');
+
+
+const secretKey = 'your-secret-key';
+
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    res.send("Express on Vercel");
 });
 
 
 app.post("/login", (req, res) => {
     // Generate random token
-    const token = crypto.randomBytes(64).toString('hex');
+    const payload = {
+        user: '51957135245'
+    };
+    const token = jwt.sign(payload, secretKey);
 
     // Random sleep time up to 1 second
-    const sleepTime = Math.random() * 400;
+    const sleepTime = Math.random() * 300;
 
     setTimeout(() => {
-        res.send({ token: token, time: sleepTime});
+        res.send({ access_token: token, expires_in: 600, refresh_token: "frt545*/", time: sleepTime });
     }, sleepTime);
 });
 
 
 app.listen(5000, () => {
-  console.log("Running on port 5000.");
+    console.log("Running on port 5000.");
 });
 
 // Export the Express API
